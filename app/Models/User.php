5b9812d903fe8c284e88534154d2a0cf347fce13
Duisengaliyev Temirlan <tduisengaliyev@klika-tech.com>
2020-11-22 17:05:10 +0600

    LinkedIn Learning. Advanced Laravel. Eloquent events

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -31,6 +32,15 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function($model){
+            $model->team_id = DB::table('teams')
+                ->inRandomOrder()->first()->id;
+        });
+    }
 
     /**
      * The attributes that should be cast to native types.
